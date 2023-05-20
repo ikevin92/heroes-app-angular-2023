987d3f3f0fc952d124e17c421fcd6bfcf925f862
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-20 18:32:05 -0500

    agregar heroe html y fin de seccion 14

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NewPageComponent {
-
+  public publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics',
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics',
+    },
+  ];
 }
