e563b95552ae771fc8ce2da69ad12fe3d8fde880
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-22 23:45:19 -0500

    checkAuthentication service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from 'src/environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -26,9 +26,23 @@ export class AuthService {
     );
   }
 
+  checkAuthentication(): Observable<boolean> {
+    if (!localStorage.getItem('token')) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap((user) => (this.user = user)),
+      map((user) => !!user),
+      catchError((err) => of(false))
+    );
+
+    // return of(true);
+  }
+
   logout() {
     this.user = undefined;
-    localStorage.removeItem('token'); // localStorage.clear();
+    // localStorage.removeItem('token'); // localStorage.clear();
     localStorage.clear();
   }
 }
